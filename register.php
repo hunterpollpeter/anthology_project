1737c13b87bdc03a26ddfb80aae0f006cfeeed5c
pollpeterh <pollpeter.h@gmail.com>
2017-09-17 16:26:43 -0500

    Added login and register pages

---
+++ register.php
@@ -0,0 +1,75 @@
+<html>
+<head>
+	<title>The Writing Anthology - Register</title>
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato|Rokkitt">
+	<link rel="stylesheet" href="../../assets/bootstrap.css">
+	<link rel="stylesheet" href="assets/style.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+</head>
+<body>
+	<nav class="navbar navbar-expand-sm navbar-dark bg-primary navbar-fixed fixed-top">
+		<a class="navbar-brand" href="index">
+			<img src="assets/CentralCollegeLogoWhite.svg">
+			<span>The Writing Anthology 2017</span>
+		</a>
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+		</button>
+		<div class="collapse navbar-collapse" id="navbarSupportedContent">
+			<ul class="navbar-nav mr-auto">
+				<li class="nav-item">
+					<a class="nav-link" href="index#contents">Contents</a>
+				</li>
+				<li class="nav-item dropdown">
+					<a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+						Archives
+					</a>
+					<div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+						<a class="dropdown-item" href="#">2016</a>
+						<a class="dropdown-item" href="#">2015</a>
+						<a class="dropdown-item" href="#">2014</a>
+					</div>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="#">Link</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="#">Link</a>
+				</li>
+			</ul>
+		</div>
+	</nav>
+	<div class="navDisplacement"></div>
+	<div class="container pt-4">
+		<div class="row">
+			<div class="col-md-12">
+				<h3>Register</h3>
+				<hr>
+				<form>
+					<div class="form-group">
+						<label for="exampleInputEmail1">Email address</label>
+						<input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+					</div>
+					<div class="form-group">
+						<label for="exampleInputPassword1">First Name</label>
+						<input type="text" class="form-control" id="FirstName" placeholder="First Name">
+					</div>
+					<div class="form-group">
+						<label for="exampleInputPassword1">Last Name</label>
+						<input type="text" class="form-control" id="LastName" placeholder="Last Name">
+					</div>
+					<div class="form-group">
+						<label for="exampleInputPassword1">Password</label>
+						<input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+					</div>
+					<div class="form-group">
+						<label for="exampleInputPassword1">Confirm Password</label>
+						<input type="password" class="form-control" id="exampleInputPassword1" placeholder="Confirm Password">
+					</div>
+					<button type="submit" class="btn btn-primary">Register</button>
+				</form>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
